e51dd0a670ae5d9132d54524a4dc4fc725af0cd8
cherishjava <92429922+cherishjava@users.noreply.github.com>
2024-03-19 21:41:43 +0800

    订单数据统计代码开发

---
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,21 @@
+package com.sky.service;
+
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
+
+import java.time.LocalDate;
+
+public interface ReportService {
+    TurnoverReportVO getTurnover(LocalDate begin, LocalDate end);
+
+    /**
+     * 根据时间区间统计用户数量
+     * @param begin
+     * @param end
+     * @return
+     */
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
+
+    OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end);
+}
